38372b0f282c6612d8beead455a4bd8827ba91e7
atlednolispe <atlednolispe@gmail.com>
2018-02-13 15:17:12 +0800

    Simple add list_display of admin. Add two bug to be corrected.

--- comment/admin.py
+++ comment/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 
+from blog.custom_site import custom_site
+
 from .models import Comment
 
 
-@admin.register(Comment)
+@admin.register(Comment, site=custom_site)
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('email', 'status')
+    search_fields = ('post__title', 'content')
+    list_display = ('post', 'content', 'nickname', 'email')
+
+    fieldsets = (
+        ('基础配置', {
+            'fields': ('post', 'content', 'status')
+        }),
+        ('高级配置', {
+            'classes': ('collapse',),  # 点击一次才展示
+            'fields': ('nickname', 'website', 'email')
+        }),
+    )
--- config/admin.py
+++ config/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
+from blog.custom_site import custom_site
+
 from .models import Link, SideBar
 
 
-@admin.register(Link)
+@admin.register(Link, site=custom_site)
 class LinkAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'href', 'weight', 'owner')
 
 
-@admin.register(SideBar)
+@admin.register(SideBar, site=custom_site)
 class SidebarAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'display_type', 'content', 'owner')
