7714e09403d50150beeff09945abe383dae347bc
atlednolispe <atlednolispe@gmail.com>
2018-02-18 22:33:31 +0800

    Custom form exhibition in the html by forms.ModelForm.

--- comment/forms.py
+++ comment/forms.py
@@ -4,6 +4,9 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    target = forms.CharField(max_length=100, widget=forms.widgets.HiddenInput)
+    content = forms.CharField(label="内容", widget=forms.widgets.Textarea(attrs={'rows': 6, 'cols': 80}))
+
     def clean_content(self):
         content = self.cleaned_data.get('content')
 
@@ -13,4 +16,4 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields = ['nickname', 'email', 'website', 'content']
+        fields = ['target', 'nickname', 'email', 'website', 'content']
